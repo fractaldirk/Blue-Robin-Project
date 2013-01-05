a97090f57c88bb0cc7a2ac041eef2eebacc23847
fractal <circle@ubuntu.(none)>
2013-01-05 16:50:17 +0100

    before pg database

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,21 @@
+class HomeController < ApplicationController
+  def index
+
+  end
+
+  def project
+
+  end
+
+  def organisations
+
+  end
+
+  def developers
+
+  end
+
+  def contact
+
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brproject::Application.config.secret_token = '78be7ed7dfb89d845b04a372c2f6b007e20247e89cb45869215d2b361c1c3ae971ccadbf00d343d4ce326abf53045a8ba7b2115254dcc382963b7dccfbf90149'
